2714f3f7e250b3de42d559acf3829cb361de4311
vanthai <vthaing@gmail>
2018-09-05 16:14:00 +0700

    Fix form bug report and functional test

--- tests/functional/BugReportFormCest.php
+++ tests/functional/BugReportFormCest.php
@@ -1,38 +1,39 @@
 <?php
 
+class BugReportFormCest
+{
 
-class BugReportFormCest {
+    public function openBugReportPageWithoutLogginIn(\FunctionalTester $I) {
+        $I->amOnPage(['assessment/index']);
+        $I->dontSee('Report a bug', 'h2');
+    }
 
-  public function openBugReportPageWithoutLogginIn(\FunctionalTester $I) {
-    $I->amOnPage(['assessment/index']);
-    $I->dontSee('Report a bug', 'h2');
-  }
+    public function submitEmptyForm(\FunctionalTester $I) {
+        $I->amLoggedInAs(1);
+        $I->amOnPage(['assessment/index']);
+        $I->submitForm('#assessment-form', []);
+        $I->expectTo('see validations errors');
+        $I->see('Report a bug', 'h2');
+        $I->see('Name cannot be blank');
+        $I->see('Email cannot be blank');
+        $I->see('Issue description cannot be blank');
+    }
 
-  public function submitEmptyForm(\FunctionalTester $I) {
-    $I->amLoggedInAs(1);
-    $I->amOnPage(['assessment/index']);
-    $I->submitForm('#assessment-form', []);
-    $I->expectTo('see validations errors');
-    $I->see('Report a bug', 'h2');
-    $I->see('Name cannot be blank');
-    $I->see('Email cannot be blank');
-    $I->see('Body cannot be blank');
-  }
+    public function submitFormSuccessfully(\FunctionalTester $I) {
+        $I->amLoggedInAs(1);
+        $I->amOnPage(['assessment/index']);
+        $I->submitForm(
+            '#assessment-form', [
+            'BugReport[name]' => 'tester',
+            'BugReport[email]' => 'tester@example.com',
+            'BugReport[issue_description]' => 'test content',
+            ]
+        );
+        $I->seeEmailIsSent();
+        $I->dontSeeElement('#assessment-form');
+        $I->see(
+            'Thank you for reporting this bug, we will respond to you via email shortly.'
+        );
+    }
 
-  public function submitFormSuccessfully(\FunctionalTester $I) {
-    $I->amLoggedInAs(1);
-    $I->amOnPage(['assessment/index']);
-    $I->submitForm(
-      '#assessment-form', [
-      'name' => 'tester',
-      'email' => 'tester@example.com',
-      'body' => 'test content',
-    ]
-    );
-    $I->seeEmailIsSent();
-    $I->dontSeeElement('#assessment-form');
-    $I->see(
-      'Thank you for reporting this bug, we will respond to you via email shortly.'
-    );
-  }
 }
--- views/assessment/index.php
+++ views/assessment/index.php
@@ -6,8 +6,10 @@ use yii\widgets\ActiveForm;
 $this->title = 'My Yii Application Test';
 ?>
 <div class="test-index">
+<?php if(!Yii::$app->session->getFlash('success', null, false)): ?>
 <h2>Report a bug</h2>
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
 </div>
+<?php endif; ?>
